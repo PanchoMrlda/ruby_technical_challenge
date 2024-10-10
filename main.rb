9e94290825918d05360743a8fe0f7de6474ab1ac
PanchoMrlda <22328157+PanchoMrlda@users.noreply.github.com>
2024-10-10 21:11:21 +0200

    Refactoring code

--- main.rb
+++ main.rb
@@ -41,54 +41,60 @@ def print_trip_data(trip_data)
   puts "\e[36m#{trip_data[:type]} from #{trip_data[:location_from]} to #{trip_data[:location_to]} at #{trip_data[:from]} #{trip_data[:hour_from]} to #{trip_data[:hour_to]}\e[0m"
 end
 
-filename = ARGV[0]
-user_origin = ENV['BASED']
+def main
+  filename = ARGV[0]
+  user_origin = ENV['BASED']
 
-begin
-  raise StandardError, 'No file provided' if filename.nil?
-  raise StandardError, 'No user origin provided' if user_origin.nil? || user_origin == ''
+  begin
+    raise StandardError, 'No file provided' if filename.nil?
+    raise StandardError, 'No user origin provided' if user_origin.nil? || user_origin == ''
 
-  reservations = []
-  completed_reservations = []
-  File.open(filename, 'r') do |f|
-    f.each_line do |line|
-      next unless line.include?('SEGMENT')
-      reservations << line_to_reservation(line)
+    reservations = []
+    completed_reservations = []
+    File.open(filename, 'r') do |f|
+      f.each_line do |line|
+        next unless line.include?('SEGMENT')
+        reservations << line_to_reservation(line)
+      end
     end
-  end
-  reservations.sort_by! { |hash| hash[:from] }
-  reservations.each do |origin|
-    next if origin[:type] == RESERVATION_TYPES[:HOTEL] || completed_reservations.include?(origin)
+    reservations.sort_by! { |hash| hash[:from] }
+    reservations.each do |origin|
+      next if origin[:type] == RESERVATION_TYPES[:HOTEL] || completed_reservations.include?(origin)
 
-    connection = get_connection(origin, reservations)
-    if connection.length == 0
-      puts "\e[36mTRIP to #{origin[:location_to]}\e[0m"
-    else
-      puts "\e[36mTRIP to #{connection.first[:location_to]}\e[0m"
-    end
-    print_trip_data(origin)
-    hotel = reservations.find { |reservation_data| reservation_data[:from].include?(origin[:from]) && reservation_data[:type] == RESERVATION_TYPES[:HOTEL] }
-    unless hotel.nil?
-      puts "\e[36m#{hotel[:type]} at #{hotel[:location_from]} on #{hotel[:from]} to #{hotel[:to]}\e[0m"
-      destination = reservations.find { |reservation_data| reservation_data[:to].include?(hotel[:to]) && reservation_data[:type] == origin[:type] && reservation_data[:location_from] == origin[:location_to] }
-    end
-    if connection.length == 0
-      destination ||= reservations.find { |reservation_data| reservation_data[:to].include?(origin[:to]) && reservation_data[:type] == origin[:type] && reservation_data[:location_from] == origin[:location_to] }
-    else
-      destination = reservations.find { |reservation_data| reservation_data[:to].include?(connection.first[:to]) && reservation_data[:type] == origin[:type] && reservation_data[:location_from] == origin[:location_to] }
-    end
-    completed_reservations << origin
-    if destination.nil?
+      connection = get_connection(origin, reservations)
+      if connection.length == 0
+        puts "\e[36mTRIP to #{origin[:location_to]}\e[0m"
+      else
+        puts "\e[36mTRIP to #{connection.first[:location_to]}\e[0m"
+      end
+      print_trip_data(origin)
+      hotel = reservations.find { |reservation_data| reservation_data[:from].include?(origin[:from]) && reservation_data[:type] == RESERVATION_TYPES[:HOTEL] }
+      unless hotel.nil?
+        puts "\e[36m#{hotel[:type]} at #{hotel[:location_from]} on #{hotel[:from]} to #{hotel[:to]}\e[0m"
+        destination = reservations.find { |reservation_data| reservation_data[:to].include?(hotel[:to]) && reservation_data[:type] == origin[:type] && reservation_data[:location_from] == origin[:location_to] }
+      end
+      if connection.length == 0
+        destination ||= reservations.find { |reservation_data| reservation_data[:to].include?(origin[:to]) && reservation_data[:type] == origin[:type] && reservation_data[:location_from] == origin[:location_to] }
+      else
+        destination = reservations.find { |reservation_data| reservation_data[:to].include?(connection.first[:to]) && reservation_data[:type] == origin[:type] && reservation_data[:location_from] == origin[:location_to] }
+      end
+      completed_reservations << origin
+      if destination.nil?
+        puts "\n"
+        next
+      end
+      completed_reservations << destination
+      print_trip_data(destination)
       puts "\n"
-      next
     end
-    completed_reservations << destination
-    print_trip_data(destination)
-    puts "\n"
+  rescue Errno::ENOENT
+    puts "\e[31mNo file found\e[0m"
+  rescue StandardError => error
+    puts "\e[31m#{error.message}\e[0m"
   end
-rescue Errno::ENOENT
-  puts "\e[31mNo file found\e[0m"
-rescue StandardError => error
-  puts "\e[31m#{error.message}\e[0m"
-  puts "\e[31m#{error.backtrace[0..15]}\e[0m"
+end
+
+# Only execute if the file is run directly
+if __FILE__ == $0
+  main
 end
